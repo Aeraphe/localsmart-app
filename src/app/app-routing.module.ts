9c0879c9a3be9949f9b58a99c49902b560ab0d58
Aeraphe <alberto.aeraph@gmail.com>
2022-02-18 20:10:47 -0300

    feat(routes): create a route for login page comp.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
   {path:"",redirectTo:"home", pathMatch:'full'},
-  {path:"home", component:DefaultComponent, children:[
-    {path:'',component:HomeComponent}
+  {path:"", component:DefaultComponent, children:[
+    {path:'home',component:HomeComponent},
+    {path:'login',component:LoginComponent}
   ]},
 
   {path:'**',component:NotFoundComponent}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [HomeComponent, NotFoundComponent],
+  declarations: [HomeComponent, NotFoundComponent, LoginComponent],
   imports: [CommonModule,SharedModule],
 })
 export class PagesModule {}
