c372f57cb70c21b2613cca6f81ce0a07fd807fbe
Aeraphe <alberto.aeraph@gmail.com>
2022-02-18 11:07:24 -0300

    feat(components): add toogle behavior on header

--- src/app/layouts/default/header/header.component.ts
+++ src/app/layouts/default/header/header.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderNavService } from '../../../shared/services/header-nav.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
+  headerState: Boolean = false;
+  constructor(private headerService: HeaderNavService) {}
 
   ngOnInit(): void {
+    this.headerService.getHeaderState().subscribe((state) => {
+      this.headerState = state;
+    });
   }
 
+  toggle = ()=>{
+    this.headerState = !this.headerState
+  }
 }
