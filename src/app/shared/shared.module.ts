698238dc0e518d713e8471eec7a8dede35a30090
Aeraphe <alberto.aeraph@gmail.com>
2022-02-18 11:08:57 -0300

    feat(services): add items to export on shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { HandburgerMenuComponent } from './components/handburger-menu/handburger-menu.component';
+import { CardComponent } from './components/card/card.component';
+import { HeaderNavService } from './services/header-nav.service';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [HandburgerMenuComponent, CardComponent],
+  imports: [CommonModule],
+  exports: [HandburgerMenuComponent, CardComponent],
+  providers:[HeaderNavService]
 })
-export class SharedModule { }
+export class SharedModule {}
