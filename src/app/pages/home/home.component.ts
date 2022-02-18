6bb055e2c2cb57adfb68d097307d6c1960a23b09
Aeraphe <alberto.aeraph@gmail.com>
2022-02-18 11:02:48 -0300

    feat(components): create home comp.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { HeaderNavService } from '../../shared/services/header-nav.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit,AfterViewInit  {
+  touch =  true;
 
-  constructor() { }
+  @ViewChild('photo')  divPhotoEl!: ElementRef<HTMLDivElement>;
+  constructor(private headerService: HeaderNavService) {
 
-  ngOnInit(): void {
   }
 
+  ngOnInit(): void {
+    
+   
+  }
+
+  ngAfterViewInit() {
+    console.log(this.divPhotoEl.nativeElement.style.setProperty('--bg-path','url("../../../assets/photos/teste.jfif")'));
+  }
+
+  closeHeader = () => {
+    if(this.touch){
+      this.headerService.toogle();
+      this.touch = false;
+
+      setTimeout(() => {
+        this.headerService.toogle();
+        this.touch = true;
+      }, 5000);
+    }
+  
+  };
 }
