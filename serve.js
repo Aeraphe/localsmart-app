655335182fffa6589830ea460098d9b2e4014396
Aeraphe <alberto.aeraph@gmail.com>
2022-02-18 11:15:58 -0300

    chore: change heroku express serve dist folder

--- serve.js
+++ serve.js
@@ -4,10 +4,10 @@ const app = express();
 const PORT = process.env.PORT || 4700;
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/smart-adv'));
 
 app.get('/*',(req,res)=>{
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/dist/smart-adv/index.html');
 })
 
 app.listen(PORT,()=>{
