c0401f8ea80387fe923b77d52fe9a00053fc2e66
Aeraphe <alberto.aeraph@gmail.com>
2022-02-19 10:00:50 -0300

    feat(modules): add admin page module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -5,9 +5,11 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { SharedModule } from '../shared/shared.module';
 import { LoginComponent } from './login/login.component';
+import { AdminModule } from './admin/admin.module';
+
 
 @NgModule({
-  declarations: [HomeComponent, NotFoundComponent, LoginComponent],
-  imports: [CommonModule,SharedModule],
+  declarations: [HomeComponent, NotFoundComponent, LoginComponent,  ],
+  imports: [CommonModule,SharedModule,AdminModule],
 })
 export class PagesModule {}
