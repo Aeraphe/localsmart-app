bce9c0cf5f300c7899825c4af396bfc1eea38cb6
Aeraphe <alberto.aeraph@gmail.com>
2022-02-19 09:59:47 -0300

    chore: remove angularfire package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,8 @@ import { AppComponent } from './app.component';
 import { DefaultModule } from './layouts/default/default.module';
 import { PagesModule } from './pages/pages.module';
 import { SharedModule } from './shared/shared.module';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideStorage,getStorage } from '@angular/fire/storage';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,14 +17,7 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
     DefaultModule,
     PagesModule,
     SharedModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    provideDatabase(() => getDatabase()),
-    provideFirestore(() => getFirestore()),
-    provideFunctions(() => getFunctions()),
-    provideStorage(() => getStorage()),
   ],
-  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
