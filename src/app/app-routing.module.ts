b8d2b729c45f0d418e046ae9331fa0d539799baa
Aeraphe <alberto.aeraph@gmail.com>
2022-02-19 10:00:28 -0300

    feat(routes): create routes for admin page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
+import { AdminComponent } from './pages/admin/admin.component';
+import { UploadManagerComponent } from './pages/admin/upload-manager/upload-manager.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
@@ -9,7 +11,10 @@ const routes: Routes = [
   {path:"",redirectTo:"home", pathMatch:'full'},
   {path:"", component:DefaultComponent, children:[
     {path:'home',component:HomeComponent},
-    {path:'login',component:LoginComponent}
+    {path:'login',component:LoginComponent},
+    {path:'admin', component:AdminComponent,children:[
+      {path:'upload-manager',component:UploadManagerComponent}
+    ]}
   ]},
 
   {path:'**',component:NotFoundComponent}
