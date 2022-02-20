c031da9d5c53b0152681b7f807af6c9db0866282
Aeraphe <alberto.aeraph@gmail.com>
2022-02-19 22:46:14 -0300

    feat(components): create add new product comp page

--- src/app/pages/admin/upload-task/upload-task.component.ts
+++ src/app/pages/admin/upload-task/upload-task.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ProductsService } from '../../../shared/services/products.service';
 
 @Component({
   selector: 'app-upload-task',
   templateUrl: './upload-task.component.html',
-  styleUrls: ['./upload-task.component.scss']
+  styleUrls: ['./upload-task.component.scss'],
 })
 export class UploadTaskComponent implements OnInit {
+  file!: File;
 
-  constructor() { }
+  uploadForm = new FormGroup({
+    file: new FormControl(null, [Validators.required]),
+    name: new FormControl(null, [Validators.required]),
+    category: new FormControl(null, [Validators.required]),
+    price: new FormControl(null, [Validators.required]),
+  });
 
-  ngOnInit(): void {
+  constructor(private productsService: ProductsService) {}
+
+  ngOnInit(): void {}
+
+  onSubmit = async () => {
+    try {
+      let formData = this.uploadForm.value;
+      let file = formData?.file;
+      await this.productsService.uploadProductImage(file, formData);
+    } catch (error) {
+      console.error(error);
+      throw new Error('File upload Error');
+    }
+  };
+
+  onFileChange(event: Event) {
+    try {
+      let target = event.target as HTMLInputElement;
+      if (target.files) {
+        const file = target.files[0];
+        this.uploadForm.patchValue({ file });
+      }
+    } catch (error) {}
   }
-
 }
