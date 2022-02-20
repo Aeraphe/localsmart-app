697c52edc120e0ac60b4add4a5700e10b10072cd
Aeraphe <alberto.aeraph@gmail.com>
2022-02-20 15:45:08 -0300

    feat(components): add overlayService on default comp

--- src/app/layouts/default/default.component.ts
+++ src/app/layouts/default/default.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { OverlayService } from '../../shared/services/overlay.service';
 
 @Component({
   selector: 'app-default',
   templateUrl: './default.component.html',
-  styleUrls: ['./default.component.scss']
+  styleUrls: ['./default.component.scss'],
 })
 export class DefaultComponent implements OnInit {
+  overlayState = true;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private overlayService: OverlayService) {
+    this.overlayService.getOverlayState().subscribe((state) => {
+      this.overlayState = state;
+    });
   }
 
+  ngOnInit(): void {}
 }
