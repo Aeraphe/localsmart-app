86e38d5ecebd61ee585d442f35a7ba3377cc4664
Aeraphe <alberto.aeraph@gmail.com>
2022-02-20 13:00:14 -0300

    feat(components): create a nav left scaffold comp.

--- src/app/layouts/default/mobile-nav/mobile-nav.component.ts
+++ src/app/layouts/default/mobile-nav/mobile-nav.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { HandburgerMenuService } from '../../../shared/services/handburger-menu.service';
 
 @Component({
   selector: 'app-mobile-nav',
   templateUrl: './mobile-nav.component.html',
-  styleUrls: ['./mobile-nav.component.scss']
+  styleUrls: ['./mobile-nav.component.scss'],
 })
 export class MobileNavComponent implements OnInit {
+  navToogle = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private handBurgerService: HandburgerMenuService) {
+    let clickHandBurger$ = this.handBurgerService.getHandBurgerClick();
+    clickHandBurger$.subscribe((click) => {
+      console.log(click);
+      this.navToogle = click;
+    });
   }
 
+  ngOnInit(): void {}
+
+  closeNav = () => {
+    this.navToogle = false;
+    this.handBurgerService.onClickHandBurger(false);
+  };
 }
