9c1fd855c64697ec7e9867e650b6ce634a19b388
Aeraphe <alberto.aeraph@gmail.com>
2022-02-20 20:49:41 -0300

    feat(components): add get products to list produtcs from firestore

--- src/app/pages/admin/product-list/product-list.component.ts
+++ src/app/pages/admin/product-list/product-list.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from "../../../shared/services/products.service";
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+
+registerLocaleData(localePt)
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'], providers: [{
+    provide: LOCALE_ID, 
+    useValue: "pt-BR"
+  }],
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+   products!:any;
+   totalProducts = 0;
+  constructor(private productService:ProductsService) { }
 
   ngOnInit(): void {
+     this.productService.getProducts().then(
+       doc=>{
+        this.products = doc
+        this.totalProducts = doc.length
+       }
+     );
+
+  
   }
+
+
 
 }
