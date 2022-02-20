c97f38bb52f2e954045cdf8669e70a283dd270e1
Aeraphe <alberto.aeraph@gmail.com>
2022-02-20 11:28:00 -0300

    feat(routes): create route for admin/product-list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,26 +2,36 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DefaultComponent } from './layouts/default/default.component';
 import { AdminComponent } from './pages/admin/admin.component';
+import { ProductListComponent } from './pages/admin/product-list/product-list.component';
 import { UploadManagerComponent } from './pages/admin/upload-manager/upload-manager.component';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
-  {path:"",redirectTo:"home", pathMatch:'full'},
-  {path:"", component:DefaultComponent, children:[
-    {path:'home',component:HomeComponent},
-    {path:'login',component:LoginComponent},
-    {path:'admin', component:AdminComponent,children:[
-      {path:'upload-manager',component:UploadManagerComponent}
-    ]}
-  ]},
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  {
+    path: '',
+    component: DefaultComponent,
+    children: [
+      { path: 'home', component: HomeComponent },
+      { path: 'login', component: LoginComponent },
+      {
+        path: 'admin',
+        component: AdminComponent,
+        children: [
+          { path: 'product-add', component: UploadManagerComponent },
+          { path: 'product-list', component: ProductListComponent },
+        ],
+      },
+    ],
+  },
 
-  {path:'**',component:NotFoundComponent}
+  { path: '**', component: NotFoundComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
