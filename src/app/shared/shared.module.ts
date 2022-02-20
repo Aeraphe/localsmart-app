3ee0eb83d2173b6be5c6b50042024bf84a4fe5f8
Aeraphe <alberto.aeraph@gmail.com>
2022-02-19 22:35:40 -0300

    feat(modules): add FirebaseAppService module to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HandburgerMenuComponent } from './components/handburger-menu/handburger-menu.component';
 import { CardComponent } from './components/card/card.component';
 import { HeaderNavService } from './services/header-nav.service';
+import { FirebaseAppService } from './services/firebase-app.service';
 
 @NgModule({
   declarations: [HandburgerMenuComponent, CardComponent],
   imports: [CommonModule],
   exports: [HandburgerMenuComponent, CardComponent],
-  providers:[HeaderNavService]
+  providers:[HeaderNavService,FirebaseAppService]
 })
 export class SharedModule {}
