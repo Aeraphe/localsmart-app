e2e4d5112e8528c53f8218b03a4d52e8eea4c81d
Aeraphe <alberto.aeraph@gmail.com>
2022-02-21 09:48:19 -0300

    feat(components): add overflowBodyService on mobile-nav comp.

--- src/app/layouts/default/mobile-nav/mobile-nav.component.ts
+++ src/app/layouts/default/mobile-nav/mobile-nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HandburgerMenuService } from '../../../shared/services/handburger-menu.service';
+import { OveflowBodyService } from '../../../shared/services/oveflow-body.service';
 
 @Component({
   selector: 'app-mobile-nav',
@@ -12,14 +13,23 @@ export class MobileNavComponent implements OnInit {
 
   constructor(
     private handBurgerService: HandburgerMenuService,
+    private overflow: OveflowBodyService,
     private router: Router
   ) {
     let clickHandBurger$ = this.handBurgerService.getHandBurgerClick();
     clickHandBurger$.subscribe((click) => {
-      console.log(click);
+      this.handleOverflowBody(click);
       this.navToogle = click;
     });
   }
+
+  handleOverflowBody = (active: boolean) => {
+    if (active) {
+      this.overflow.activeOverflowBody();
+    } else {
+      this.overflow.removeOverflowBody();
+    }
+  };
 
   ngOnInit(): void {}
 
@@ -31,6 +41,5 @@ export class MobileNavComponent implements OnInit {
   onClick = (route: string) => {
     this.handBurgerService.onClickHandBurger(false);
     this.router.navigate([route]);
-    
   };
 }
