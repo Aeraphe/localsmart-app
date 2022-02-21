50814b2586fb061d610cbfda5034ebf65ebea81c
Aeraphe <alberto.aeraph@gmail.com>
2022-02-21 12:40:57 -0300

    feat(components): add delete function and alert comp for delete selected product

--- src/app/pages/admin/product-list/product-list.component.ts
+++ src/app/pages/admin/product-list/product-list.component.ts
@@ -1,36 +1,55 @@
-import { Component, OnInit } from '@angular/core';
-import { ProductsService } from "../../../shared/services/products.service";
+import { Component, OnInit, Output } from '@angular/core';
+import { ProductsService } from '../../../shared/services/products.service';
+import { AlertService } from '../../../shared/services/alert.service';
 import { LOCALE_ID } from '@angular/core';
 import localePt from '@angular/common/locales/pt';
-import {registerLocaleData} from '@angular/common';
+import { registerLocaleData } from '@angular/common';
 
-registerLocaleData(localePt)
+registerLocaleData(localePt);
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss'], providers: [{
-    provide: LOCALE_ID, 
-    useValue: "pt-BR"
-  }],
+  styleUrls: ['./product-list.component.scss'],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt-BR',
+    },
+  ],
 })
 export class ProductListComponent implements OnInit {
+  @Output() openAlert: boolean = false;
 
-   products!:any;
-   totalProducts = 0;
-  constructor(private productService:ProductsService) { }
+  selectedProduct: any;
+  alertDeleteText: string = '';
+  products!: any;
+  totalProducts = 0;
+  constructor(
+    private productService: ProductsService,
+    private alertService: AlertService
+  ) {}
 
   ngOnInit(): void {
-     this.productService.getProducts().then(
-       doc=>{
-        this.products = doc
-        this.totalProducts = doc.length
-       }
-     );
-
-  
+    this.handleGetProducts();
   }
 
+  private handleGetProducts = () => {
+    this.productService.getProducts().then((doc) => {
+      this.products = doc;
+      this.totalProducts = doc.length;
+    });
+  };
 
+  openDeleteAlert = (productSelected: any) => {
+    this.selectedProduct = productSelected;
+    this.alertDeleteText = 'Tem certeza que deseja apagar o Produto?';
+    this.openAlert = true;
+    this.alertService.openAlert();
+  };
 
+  deleteProduct = () => {
+    this.productService.deleteProduct(this.selectedProduct.id);
+    this.handleGetProducts();
+  };
 }
