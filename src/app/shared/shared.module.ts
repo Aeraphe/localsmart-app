6e86ca5d0795e101289afef148e1ffd57c9d35ad
Aeraphe <alberto.aeraph@gmail.com>
2022-02-21 17:23:26 -0300

    feat(modules): add Admin guard to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,12 @@ import { HeaderNavService } from './services/header-nav.service';
 import { FirebaseAppService } from './services/firebase-app.service';
 import { AlertComponent } from './components/alert/alert.component';
 import { OverlayService } from './services/overlay.service';
+import { AdminGuard } from './aguards/admin.guard';
 
 @NgModule({
   declarations: [HandburgerMenuComponent, CardComponent, AlertComponent],
   imports: [CommonModule],
   exports: [HandburgerMenuComponent, CardComponent, AlertComponent],
-  providers: [HeaderNavService, FirebaseAppService, OverlayService],
+  providers: [HeaderNavService, FirebaseAppService, OverlayService,AdminGuard],
 })
 export class SharedModule {}
