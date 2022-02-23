c81ea14e8b43ae18915725d9834f09fc694f9b38
Aeraphe <alberto.aeraph@gmail.com>
2022-02-23 15:04:15 -0300

    feat(modules): add routermodule and currency pipe to shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, CurrencyPipe } from '@angular/common';
 import { HandburgerMenuComponent } from './components/handburger-menu/handburger-menu.component';
 import { CardComponent } from './components/card/card.component';
 import { HeaderNavService } from './services/header-nav.service';
@@ -9,11 +9,13 @@ import { OverlayService } from './services/overlay.service';
 import { AdminGuard } from './aguards/admin.guard';
 import { CarousellComponent } from './components/carousell/carousell.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [HandburgerMenuComponent, CardComponent, AlertComponent,  CarousellComponent, ProductListComponent],
-  imports: [CommonModule],
+  imports: [CommonModule,RouterModule],
   exports: [HandburgerMenuComponent, CardComponent, AlertComponent,CarousellComponent,ProductListComponent],
-  providers: [HeaderNavService, FirebaseAppService, OverlayService,AdminGuard],
+  providers: [HeaderNavService, FirebaseAppService, OverlayService,AdminGuard,CurrencyPipe],
 })
 export class SharedModule {}
