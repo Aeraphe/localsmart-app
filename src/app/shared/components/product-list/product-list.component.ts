11471f5655dabf4bf78d6d999e516ebe5f734839
Aeraphe <alberto.aeraph@gmail.com>
2022-02-23 11:03:21 -0300

    feat(components): add whatsapp btn o product-list for share the product

--- src/app/shared/components/product-list/product-list.component.ts
+++ src/app/shared/components/product-list/product-list.component.ts
@@ -5,6 +5,8 @@ import {
   SimpleChanges,
   OnChanges,
 } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-product-list',
@@ -13,23 +15,39 @@ import {
 })
 export class ProductListComponent implements OnInit, OnChanges {
   @Input() products: any[] = [];
+  @Input() title: string = 'Ofertas';
 
   selectedProduct: any;
   alertDeleteText: string = '';
   displayList = false;
   totalProducts = 0;
-  constructor() {}
+  constructor(private sanitizer: DomSanitizer) {}
 
   ngOnChanges(changes: SimpleChanges): void {
     let currValueTotalItems = changes['products'].currentValue.length;
 
     if (currValueTotalItems != 0) {
-      
       this.displayList = true;
-    }else{
+    } else {
       this.displayList = false;
     }
   }
 
   ngOnInit(): void {}
+
+  encodeURI = (product: Product) => {
+    let baseURI = 'whatsapp://send?text=';
+    let route = 'https://localsmart-app.web.app/product-details/';
+
+    let uri = baseURI + route + product.id;
+
+    let saftURI: any = this.sanitizer.bypassSecurityTrustUrl(uri);
+    saftURI.changingThisBreaksApplicationSecurity =
+      uri +
+      '%0a %0a %e2%98%85 ' +
+      product.name +
+      '%0a %e2%86%92 ' +
+      product.price;
+    return saftURI;
+  };
 }
