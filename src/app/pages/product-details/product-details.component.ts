39283216f5b0e2778d3aafb92ac380775639574c
Aeraphe <alberto.aeraph@gmail.com>
2022-02-23 15:11:22 -0300

    feat(components): update product-details for receive route param and show the select product

--- src/app/pages/product-details/product-details.component.ts
+++ src/app/pages/product-details/product-details.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/interfaces/product';
+import { ProductsService } from '../../shared/services/products.service';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrls: ['./product-details.component.scss']
+  styleUrls: ['./product-details.component.scss'],
 })
 export class ProductDetailsComponent implements OnInit {
+  selectedId: number = 0;
+  product!: any | null;
+  products: any = [];
+  constructor(
+    private route: ActivatedRoute,
+    private productsService: ProductsService
+  ) {}
 
-  constructor() { }
+  async ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
 
-  ngOnInit(): void {
+
+
+    this.route.paramMap.subscribe(async (paramMap) => {
+      let id = paramMap.get('id');
+      this.product = await this.productsService.getProductById(id);
+    });
+
+    this.products = await this.productsService.getProducts();
   }
-
 }
