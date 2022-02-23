e79a2cc2103386292285479557424fc1f0f3c4f0
Aeraphe <alberto.aeraph@gmail.com>
2022-02-23 10:57:55 -0300

    feat(interfaces): add new props to product interface

--- src/app/interfaces/product.ts
+++ src/app/interfaces/product.ts
@@ -1,8 +1,13 @@
 export interface Product {
+  id?: string;
   url?: string;
   file_name?: string;
   category: string;
   name: string;
   price?: number;
   props?: [{ name: string; value: string }];
+  sold?: boolean;
+  promo?: boolean;
+  payment_method?: string;
+  condition?: boolean;
 }
