e8b2aa5324893abeef0291813191ff50b1906f21
Aeraphe <alberto.aeraph@gmail.com>
2022-02-24 09:23:04 -0300

    feat(components): add routoer location to admin page to get static route data and show to the user where is he

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { RouteLocationService } from '../../shared/services/route-location.service';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss'],
 })
 export class AdminComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private locationService: RouteLocationService,
+    private routeActive: ActivatedRoute
+  ) {
+    this.locationService.setRouteLocation(
+      this.routeActive.snapshot.data['info']
+    );
   }
 
+  ngOnInit(): void {}
 }
