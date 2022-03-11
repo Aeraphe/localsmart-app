1b6f67d28e797c0b8e7c537797508f0420766453
Aeraphe <alberto.aeraph@gmail.com>
2022-03-11 19:10:27 -0300

    feat(componets): create a link to send product list to whatsapp

--- src/app/pages/admin/product-wha-list/product-wha-list.component.ts
+++ src/app/pages/admin/product-wha-list/product-wha-list.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductsService } from '../../../shared/services/products.service';
+import { WhatsappService } from '../../../shared/services/whatsapp.service';
 
 @Component({
   selector: 'app-product-wha-list',
   templateUrl: './product-wha-list.component.html',
-  styleUrls: ['./product-wha-list.component.scss']
+  styleUrls: ['./product-wha-list.component.scss'],
 })
 export class ProductWhaListComponent implements OnInit {
+  products: [] = [];
+  productCategory: any[] = [];
+  productWhatsappListHref = "";
 
-  constructor() { }
+  constructor(private productService: ProductsService,private whatsService:WhatsappService) {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    let productCategory = await this.productService.getProductCategory();
+
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+
+     this.productWhatsappListHref = this.whatsService.createProductList(productCategory,data)
+      //this.filterProductCategory(productCategory);
+    });
   }
 
+  private filterProductCategory = (productCategory: string[]) => {
+    this.productCategory = productCategory.filter((item: any) => {
+      return this.products.reduce((prev: any, pro: any) => {
+        if (item == pro.category) {
+          prev = true;
+        } else {
+          prev = false;
+        }
+        return prev;
+      }, false);
+    });
+    console.log(this.productCategory);
+  };
 }
