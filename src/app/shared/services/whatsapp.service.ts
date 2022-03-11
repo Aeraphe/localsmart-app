88d956a5e6a6abdb5f930b4f16a71dc08773943e
Aeraphe <alberto.aeraph@gmail.com>
2022-03-11 19:09:30 -0300

    feat(services): create a whatsapp service for make messages to share in the app

---
+++ src/app/shared/services/whatsapp.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { Product } from 'src/app/interfaces/product';
+import { DomSanitizer } from '@angular/platform-browser';
+import { CurrencyPipe } from '@angular/common';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WhatsappService {
+  space = ' %0a ';
+  star = ' %e2%98%85 ';
+  arrow = ' %e2%86%92 ';
+
+  constructor(
+    private sanitizer: DomSanitizer,
+    private currencyService: CurrencyPipe
+  ) {}
+
+  createProductList = (productCategories: any[], products: any[]) => {
+    let baseURI: string = 'whatsapp://send?text=';
+    let saftURI: any = this.sanitizer.bypassSecurityTrustUrl(baseURI);
+    let list = '';
+    let productCategory = '';
+
+    productCategories.forEach((category) => {
+      productCategory = '       ' + category + this.space + this.space;
+      let productItems = '';
+      products.forEach((product) => {
+        if (category == product.category) {
+          productItems =
+            productItems +
+            this.star +
+            product.name + " " +
+            this.currencyService.transform(product.price, 'BRL') +
+            this.space;
+        }
+      });
+
+      if (productItems != '') {
+        list = list + productCategory + productItems;
+      }
+    });
+
+    console.log(list);
+
+    saftURI.changingThisBreaksApplicationSecurity = baseURI + list;
+
+    return saftURI;
+  };
+
+  createProductShareURI = (
+    product: Product,
+
+    route: string = 'https://localsmart-app.web.app/product-details/'
+  ) => {
+    let baseURI: string = 'whatsapp://send?text=';
+    let uri = baseURI + route + product.id;
+
+    let saftURI: any = this.sanitizer.bypassSecurityTrustUrl(uri);
+    saftURI.changingThisBreaksApplicationSecurity =
+      uri +
+      '%0a %0a %e2%98%85 ' +
+      product.name +
+      '%0a %e2%86%92 ' +
+      this.currencyService.transform(product.price, 'BRL');
+    return saftURI;
+  };
+}
