1f938b00058159bf2ab081146bcd60f93185be81
Aeraphe <alberto.aeraph@gmail.com>
2022-03-11 17:49:20 -0300

    feat(services): update product list msg for whatsapp

--- src/app/shared/services/whatsapp.service.ts
+++ src/app/shared/services/whatsapp.service.ts
@@ -10,40 +10,69 @@ export class WhatsappService {
   space = ' %0a ';
   star = ' %e2%98%85 ';
   arrow = ' %e2%86%92 ';
+  symbols = {
+    'money-bag': '%f0%9f%92%b0 ',
+    space: ' %20 ',
+    'line-break': ' %0a ',
+    store: ' %f0%9f%8f%a0 ',
+    list: ' %f0%9f%93%8b ',
+    phone: '%e2%98%8e ',
+    fire:'%f0%9f%94%a5 '
+  };
 
   constructor(
     private sanitizer: DomSanitizer,
     private currencyService: CurrencyPipe
   ) {}
 
-  createProductList = (productCategories: any[], products: any[],store:string = "LocalSmart") => {
+  createProductList = (
+    productCategories: any[],
+    products: any[],
+    store: string = 'LocalSmart'
+  ) => {
     let baseURI: string = 'whatsapp://send?text=';
     let saftURI: any = this.sanitizer.bypassSecurityTrustUrl(baseURI);
     let list = '';
     let productCategory = '';
+    let now = new Date();
+    let listDate = now.getDay() + '/' + now.getDate() + '/' + now.getFullYear();
 
-    productCategories.forEach((category) => {
-      productCategory = ' &#8195 ' + this.arrow + category + this.space + this.space;
+    productCategories.forEach((category: string) => {
+      productCategory =
+        '' + this.arrow + category.toUpperCase() + this.space + this.space;
       let productItems = '';
       products.forEach((product) => {
         if (category == product.category) {
           productItems =
             productItems +
             this.star +
-            product.name + " " +
+            product.name +
+            ' ' +
+            this.symbols['money-bag'] +
             this.currencyService.transform(product.price, 'BRL') +
             this.space;
         }
       });
 
       if (productItems != '') {
-        list =  list + productCategory + productItems;
+        list = list + productCategory + productItems;
       }
     });
 
     console.log(list);
 
-    saftURI.changingThisBreaksApplicationSecurity = baseURI + store + " " + Date() + this.space + "Lista de Produtos:" + this.space+  list;
+    saftURI.changingThisBreaksApplicationSecurity =
+      baseURI +
+      this.symbols.store +
+      store +
+      ' ' +
+      listDate +
+      this.symbols['line-break']  +
+      "Vendas:" + '(31) 9999-9999' + this.symbols['line-break'] +  this.symbols['line-break'] +
+      'Lista de Produtos:' +
+      this.symbols['line-break'] +
+      this.symbols['line-break'] +
+      list;
 
     return saftURI;
   };
