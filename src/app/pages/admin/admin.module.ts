85582653f30c4c39d4804c444682bd806191fdbf
Aeraphe <alberto.aeraph@gmail.com>
2022-03-18 16:40:28 -0300

    feat(modules): add ChangePriceComponent in admin module

--- src/app/pages/admin/admin.module.ts
+++ src/app/pages/admin/admin.module.ts
@@ -10,9 +10,12 @@ import { ProductListComponent } from './product-list/product-list.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatButtonModule } from '@angular/material/button';
 import { ProductUpdateComponent } from './product-list/product-update/product-update.component';
 import { ProductWhaListComponent } from './product-wha-list/product-wha-list.component';
 import { StoreComponent } from './store/store.component';
+import { ChangePriceComponent } from './product-wha-list/change-price/change-price.component';
 
 @NgModule({
   declarations: [
@@ -23,6 +26,7 @@ import { StoreComponent } from './store/store.component';
     ProductUpdateComponent,
     ProductWhaListComponent,
     StoreComponent,
+    ChangePriceComponent,
   ],
   imports: [
     CommonModule,
@@ -33,6 +37,8 @@ import { StoreComponent } from './store/store.component';
     MatSlideToggleModule,
     MatCheckboxModule,
     MatDialogModule,
+    MatSliderModule,
+    MatButtonModule,
   ],
 })
 export class AdminModule {}
