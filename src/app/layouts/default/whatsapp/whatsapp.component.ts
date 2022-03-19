826ec472e93941723028e6e829502e168f4d2dfe
Aeraphe <alberto.aeraph@gmail.com>
2022-03-19 14:53:04 -0300

    feat(components): create a whatsapp float btn

---
+++ src/app/layouts/default/whatsapp/whatsapp.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-whatsapp',
+  templateUrl: './whatsapp.component.html',
+  styleUrls: ['./whatsapp.component.scss'],
+})
+export class WhatsappComponent implements OnInit {
+  constructor(private sanitizer: DomSanitizer) {}
+  icon = 'fa-brands fa-whatsapp';
+
+  ngOnInit(): void {
+    this.changeIcon();
+  }
+
+  encodeURI = () => {
+    let baseURI = 'whatsapp://send?phone=5531994292827&text=';
+    let route = 'https://localsmart-app.web.app %0a %0a';
+    let info = '*Ola!!* _Gostaria de informações sobre;_ %0a %0a';
+
+    let uri = baseURI + route;
+
+    let saftURI: any = this.sanitizer.bypassSecurityTrustUrl(uri);
+    saftURI.changingThisBreaksApplicationSecurity = uri + info;
+    return saftURI;
+  };
+
+  private changeIcon = () => {
+    let toogle = true;
+    setInterval(() => {
+      if (toogle) {
+        this.icon = 'fa-solid fa-headset small';
+      } else {
+        this.icon = 'fa-brands fa-whatsapp';
+      }
+      toogle = !toogle;
+    }, 5500);
+  };
+}
