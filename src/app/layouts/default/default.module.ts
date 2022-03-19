63e19ad2564f5a59afbf6cb495683363b2495359
Aeraphe <alberto.aeraph@gmail.com>
2022-03-19 14:53:50 -0300

    feat(modules): include WhatsappComponent on module

--- src/app/layouts/default/default.module.ts
+++ src/app/layouts/default/default.module.ts
@@ -5,11 +5,12 @@ import { RouterModule } from '@angular/router';
 import { HeaderComponent } from 'src/app/layouts/default/header/header.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { MobileNavComponent } from './mobile-nav/mobile-nav.component';
+import { WhatsappComponent } from './whatsapp/whatsapp.component';
 
 
 
 @NgModule({
-  declarations: [DefaultComponent,HeaderComponent, MobileNavComponent],
+  declarations: [DefaultComponent,HeaderComponent, MobileNavComponent, WhatsappComponent],
   imports: [
     CommonModule,
     RouterModule,
