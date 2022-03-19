c70f9705cd2608a1614885c860853696483d8aac
Aeraphe <alberto.aeraph@gmail.com>
2022-03-19 11:04:25 -0300

    feat(compoents): update the filtrer product by category for list them

--- src/app/pages/product-list-page/product-list-page.component.ts
+++ src/app/pages/product-list-page/product-list-page.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
 import { ProductsService } from 'src/app/shared/services/products.service';
 import { RouteLocationService } from 'src/app/shared/services/route-location.service';
+import { zip } from 'rxjs';
 
 @Component({
   selector: 'app-product-list-page',
@@ -13,31 +14,50 @@ export class ProductListPageComponent implements OnInit {
   category: string | null = '';
   totalProducts: number = 0;
   products: Product[] = [];
+  productsFiltred: Product[] = [];
   constructor(
     private activeRoute: ActivatedRoute,
     private router: Router,
     private productsService: ProductsService,
     private locationService: RouteLocationService
   ) {
-    this.category = this.activeRoute.snapshot.paramMap.get('cat');
-    this.locationService.setRouteLocation(this.category?.toUpperCase() || 'Produtos' );
+    this.activeRoute.params.subscribe((data: any) => {
+      if (data.cat) {
+        this.category = data.cat;
+        this.locationService.setRouteLocation(
+          this.category?.toUpperCase() || 'Produtos'
+        );
+        this.getFilterProductsByCat(data.cat);
+      }
+    });
+
+    let zipObservables = zip(
+      this.productsService.getProducts(),
+      this.activeRoute.params
+    );
+
+    zipObservables.subscribe((data: any[]) => {
+      this.products = data[0];
+      this.category = data[1].cat;
+      if (this.category) {
+        this.getFilterProductsByCat(this.category);
+      }
+    });
   }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
 
-    if (this.category != null) {
-      let cat = this.category;
-      this.productsService.getProducts().subscribe((data) => {
-        this.products = data.filter((item: Product) => {
-          if (item.category.includes(cat)) {
-            return true;
-          } else {
-            return false;
-          }
-        });
+  private getFilterProductsByCat = (category: string) => {
+    if (category != null && this.products.length > 0) {
+      this.productsFiltred = this.products.filter((item: Product) => {
+        if (item.category.includes(category)) {
+          return true;
+        } else {
+          return false;
+        }
       });
     }
-  }
+  };
 
   navigate = (id: string | undefined) => {
     if (id !== undefined) {
