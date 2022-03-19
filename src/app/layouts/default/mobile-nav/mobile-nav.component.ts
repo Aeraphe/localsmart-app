aeeb8d42b367a9f3d04aefd7ce7080f9a05b035d
Aeraphe <alberto.aeraph@gmail.com>
2022-03-19 11:02:30 -0300

    feat(components): automate the creation of products btn on nav menu

--- src/app/layouts/default/mobile-nav/mobile-nav.component.ts
+++ src/app/layouts/default/mobile-nav/mobile-nav.component.ts
@@ -3,6 +3,8 @@ import { Router } from '@angular/router';
 import { HandburgerMenuService } from '../../../shared/services/handburger-menu.service';
 import { OveflowBodyService } from '../../../shared/services/oveflow-body.service';
 import { AuthenticationService } from '../../../shared/services/authentication.service';
+import { ProductsService } from 'src/app/shared/services/products.service';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-mobile-nav',
@@ -13,14 +15,24 @@ export class MobileNavComponent implements OnInit {
   navToogle = false;
   user: any;
 
+  productsMenuBtn: any[] = [];
+
   constructor(
     private handBurgerService: HandburgerMenuService,
     private overflow: OveflowBodyService,
     private router: Router,
-    private authService: AuthenticationService
+    private authService: AuthenticationService,
+    private productService: ProductsService
   ) {
     this.handleClickEventOnHandBurgerBtn();
     this.handleUserLogin();
+  }
+
+  async ngOnInit(): Promise<void> {
+    let productCategories: any = await this.productService.getProductCategory();
+    this.productService.getProducts().subscribe((products) => {
+      this.productsMenuBtn =  this.getMenuBtn(productCategories, products);
+    });
   }
 
   private handleClickEventOnHandBurgerBtn() {
@@ -45,8 +57,6 @@ export class MobileNavComponent implements OnInit {
     }
   };
 
-  ngOnInit(): void {}
-
   closeNav = () => {
     this.navToogle = false;
     this.handBurgerService.onClickHandBurger(false);
@@ -55,5 +65,23 @@ export class MobileNavComponent implements OnInit {
   onClick = (route: string) => {
     this.handBurgerService.onClickHandBurger(false);
     this.router.navigate([route]);
+  };
+
+  private getMenuBtn = (productCategories: Product[], products: Product[]) => {
+    let btnProducts = productCategories.filter((category: any) => {
+      let count = 0;
+      let findCategory = false;
+      while (count < products.length && !findCategory) {
+        let product = products[count];
+
+        if (product.category == category.name) {
+          findCategory = true;
+        }
+        count++;
+      }
+
+      return findCategory;
+    });
+    return btnProducts;
   };
 }
