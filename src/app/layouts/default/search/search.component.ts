e455952318c8c166b7885e2eb27951259868423b
Aeraphe <alberto.aeraph@gmail.com>
2022-03-21 15:47:09 -0300

    feat(components): add fuzzy filter on search comp.

--- src/app/layouts/default/search/search.component.ts
+++ src/app/layouts/default/search/search.component.ts
@@ -5,6 +5,13 @@ import {
   ViewChild,
   AfterViewInit,
 } from '@angular/core';
+import { Router } from '@angular/router';
+
+import Fuse from 'fuse.js';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import { Product } from 'src/app/interfaces/product';
+import { LoaderService } from 'src/app/shared/services/loader.service';
+import { ProductsService } from 'src/app/shared/services/products.service';
 import { SearchService } from 'src/app/shared/services/search.service';
 
 @Component({
@@ -14,10 +21,51 @@ import { SearchService } from 'src/app/shared/services/search.service';
 })
 export class SearchComponent implements OnInit, AfterViewInit {
   showSearch = false;
-  @ViewChild('search_input') searchInputRef!: ElementRef<HTMLInputElement>;
-  constructor(private serachService: SearchService) {}
 
-  ngOnInit(): void {}
+  private subJectProductFiltred: Subject<Product[]> = new Subject();
+  productFiltred$ = this.subJectProductFiltred.asObservable();
+  products = [];
+  search: string = '';
+  search$: Subject<string> = new Subject();
+  fuzzySearch: any;
+  @ViewChild('search_input') searchInputRef!: ElementRef<HTMLInputElement>;
+  constructor(
+    private serachService: SearchService,
+    private productService: ProductsService,
+    private router: Router,
+    private loadingService: LoaderService
+  ) {
+    this.productService.getProducts().subscribe((data) => {
+      this.products = data;
+      this.fuzzySearch = new Fuse(data, {
+        keys: ['name'],
+        threshold: 0,
+        isCaseSensitive: false,
+        includeScore: true,
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    this.serachFilterHandler();
+  }
+
+  private serachFilterHandler = () => {
+    this.search$
+      .pipe(debounceTime(1500), distinctUntilChanged())
+      .subscribe((data) => {
+        let productFiltred = this.fuzzySearch
+          .search(data)
+          .reduce((pre: any, next: any) => {
+            pre = pre || [];
+            pre.push(next.item);
+            return pre;
+          }, []);
+        this.subJectProductFiltred.next(productFiltred);
+        this.loadingService.setLoaderState(false);
+        this.searchInputRef.nativeElement.blur();
+      });
+  };
   ngAfterViewInit() {
     this.serachService.searchAction().subscribe((action) => {
       this.showSearch = action;
@@ -35,5 +83,19 @@ export class SearchComponent implements OnInit, AfterViewInit {
 
   setFocous = () => {
     this.searchInputRef.nativeElement.focus();
+  };
+
+  searchProduct = (data: any) => {
+    if (data.target.value != '') {
+      this.loadingService.setLoaderState(true);
+      this.search$.next(data.target.value);
+    }
+  };
+
+  navigate = (id: string | undefined) => {
+    if (id !== undefined) {
+      this.toogle();
+      this.router.navigate(['product-details/' + id]);
+    }
   };
 }
